22f1fbac2ade8e04e44099accec3ce240555dc68
sagehartly <sage.hartly@gmail.com>
2016-03-11 21:09:00 -0500

    fixed js issues

--- js/main.js
+++ js/main.js
@@ -4,12 +4,17 @@ var $img = $('<img>');
 var $h2 = $('<h2>');
 var $p = $('<p>');
 
-grains.forEach(function(item) {
-$ul.append($li);
-$img.attr('src', 'images/' + item.img);
-$li.append($img);
-$h2.html(item.name);
-$li.append($h2);
-$p.html(item.desc);
-$li.append($p);
-});
+var listGrains = function (grains) {
+  grains.forEach(function (item) {
+
+    $ul.append($li);
+    $img.attr('src', 'images/' + item.img);
+    $li.append($img);
+    $h2.html(item.name);
+    $li.append($h2);
+    $p.html(item.desc);
+    $li.append($p);
+  });
+};
+
+listGrains(grains);
