653267807084a6e6a025c71492220cf1de120b50
sagehartly <sage.hartly@gmail.com>
2016-03-11 21:28:53 -0500

    Put variables in the 'for each'

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
-var $ul = $('ul');
-var $li = $('<li>');
-var $img = $('<img>');
-var $h2 = $('<h2>');
-var $p = $('<p>');
+var listGrains = ['Peanuts', 'Beans', 'Lentils'];
 
-var listGrains = function (grains) {
-  grains.forEach(function (item) {
+grains.forEach(function (item) {
 
-    $ul.append($li);
-    $img.attr('src', 'images/' + item.img);
-    $li.append($img);
-    $h2.html(item.name);
-    $li.append($h2);
-    $p.html(item.desc);
-    $li.append($p);
-  });
-};
+  var $ul = $('ul');
+  var $li = $('<li>');
+  var $img = $('<img>');
+  var $h2 = $('<h2>');
+  var $p = $('<p>');
 
-listGrains(grains);
+  $ul.append($li);
+  $img.attr('src', 'images/' + item.img);
+  $li.append($img);
+  $h2.html(item.name);
+  $li.append($h2);
+  $p.html(item.desc);
+  $li.append($p);
+});
