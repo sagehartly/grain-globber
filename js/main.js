539e03d5329c47f9d95b491010529220f0b64a55
sagehartly <sage.hartly@gmail.com>
2016-03-11 20:33:45 -0500

    Setting up variables

--- js/main.js
+++ js/main.js
@@ -4,4 +4,11 @@ var $img = $('<img>');
 var $h2 = $('<h2>');
 var $p = $('<p>');
 
-$('ul').append($li);
+grains.forEach(function(item) {
+$ul.append($li);
+$img.attr('src', 'images/' + item.img)
+$li.append($img);
+$h2.html(item.name)
+$li.append($h2);
+$li.append($p);
+});
